68020b347ae3999d01e56f79b9f74dfd081ec622
dangqihe <dangqihe@baidu.com>
2018-04-03 17:31:06 +0800

    dueros

---
+++ dueros-bot/src/main/java/com/dang/demo/duerosbot/DuerosBotApplication.java
@@ -0,0 +1,12 @@
+package com.dang.demo.duerosbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuerosBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuerosBotApplication.class, args);
+	}
+}
